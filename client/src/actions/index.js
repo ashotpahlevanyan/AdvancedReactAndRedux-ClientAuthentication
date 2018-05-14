dcd4f88b63cc74e496205d8326720e66128db09e
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-05-14 23:02:58 +0400

    signin action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,13 @@
+import axios from 'axios';
 
-
+const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }){
 	return function(dispatch) { // we get arbitrary access to dispatch function with reduxThunk
 	                            // that we injected inside main src file
 		// Submit email/password to server
+
+		axios.post(`${ROOT_URL}/signin`, { email, password });
 		// If the request is good
 		// - Update state to indicate user is authenticated
 		// - Save the JWT token
