47e818315eec92659a0a38303eb54f7fc788987f
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-05-16 18:55:14 +0400

    something wrong with error rendering

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { browserHistory } from 'react-router';
 import {
 	AUTH_USER,
-	UNAUTH_USER,
-	AUTH_ERROR
+	AUTH_ERROR,
+	UNAUTH_USER
 } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
@@ -11,7 +11,6 @@ const ROOT_URL = 'http://localhost:3090';
 export function signinUser({ email, password }){
 	return function(dispatch) { // we get arbitrary access to dispatch function with reduxThunk
 	                            // that we injected inside main src file
-
 		// Submit email/password to server
 		axios.post(`${ROOT_URL}/signin`, { email, password })
 			.then(response => {
@@ -33,11 +32,23 @@ export function signinUser({ email, password }){
 }
 
 
+export function signupUser({email, password}) {
+	return function(dispatch) {
+		axios.post(`${ROOT_URL}/signup`, {email, password})
+			.then(response => {
+				dispatch({type: AUTH_USER});
+				localStorage.setItem('token', response.data.token);
+				browserHistory.push('/feature');
+			})
+			.catch(response => dispatch(authError(response.error)));
+	}
+}
+
 export function authError(error) {
 	return {
 		type: AUTH_ERROR,
 		payload: error
-	}
+	};
 }
 
 export function signoutUser() {
